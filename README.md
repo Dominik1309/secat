2b5ce608fc3ddf7e11f2c16e3c23d9d1641a8433
starvis <nichil.strasser@gmail.com>
2020-04-12 16:29:44 +0200

    added portscan.py and edited README.md

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # Setting up venv
 
 Make sure Python is installed. You can get it from Microsoft Store as well.
-The store installs it here 'C:\Users\nichil\AppData\Local\Programs\Python\Python38\'
+The store installs it here 'C:\Users\<User>\AppData\Local\Programs\Python\Python38\'
 
 ```cmd
 cd where\ever\you\want\to\create\the\virtual_enviroment
-set PYTHON_HOME=C:\Users\nichil\AppData\Local\Programs\Python\Python38
+set PYTHON_HOME=C:\Users\<User>\AppData\Local\Programs\Python\Python38
 %PYTHON_HOME%\python -m venv secat\venv
 
 cd secat
 
 ```
+
 Activate the just created pythond environment
 ```console
-C:\Users\nichil\source\secat>.\Scripts\activate
-(secat) C:\Users\nichil\source\secat>
+C:\Users\<User>\source\secat>.\venv\Scripts\activate
+(secat) C:\Users\<User>\source\secat>
 ```
 
+Now install requirements
+```cmd
+pip install -r .\lab4\reqirements.txt
+```
--- lab4/portscan.py
+++ lab4/portscan.py
@@ -1,7 +1,44 @@
-# imports
+from scapy.config import conf
+from scapy.layers.inet import IP, TCP
+from scapy.sendrecv import sr
 
 __author__ = "nichil"
 __date__ = "12.04.2020"
 __github__ = "https://github.com/starvis"
 
-# description
+
+def scan_tcp_port_range(ip, port):
+    packages = IP(dst=ip, ttl=43) / TCP(dport=port)
+    result = sr(packages, timeout=2)
+    return result
+
+
+def main():
+    # print(conf)
+    # enable debug log
+    conf.logLevel = 10
+    ip = '192.168.178.22'
+
+    # port syntax is either single 443 or range (1, 1024)
+    results, unanswered = scan_tcp_port_range(ip=ip, port=(5000, 10000))
+
+    print(results)
+
+    # for i in result:
+    #     if i.listname == 'Results':
+    #         print("Replies: ", len(i))
+    #         for j in i:
+    #             print(j)
+
+    # for result in results:
+    #     for layer in result:
+    #         if layer.getlayer('IP').flags & 2:
+    #             print(layer.getlayer('TCP').sport)
+
+    for pout, pin in results:
+        if pin.getlayer('IP').flags & 2:
+            print(pin.getlayer('TCP').sport)
+
+
+if __name__ == "__main__":
+    main()
